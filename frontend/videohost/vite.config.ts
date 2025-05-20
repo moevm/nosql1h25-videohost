0ebd5cc3a8c83829e3a88046c094c1490cd6f5f1
kirywa <kirillanisimov1337@yandex.ru>
2025-05-20 22:41:01 +0300

    init frontend

---
+++ frontend/videohost/vite.config.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+          '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+          '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
+          '@entities': fileURLToPath(new URL('./src/entities', import.meta.url)),
+          '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+          '@widgets': fileURLToPath(new URL('./src/widgets', import.meta.url)),
+          '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+          '@processes': fileURLToPath(new URL('./src/processes', import.meta.url)),
+        },
+  },
+})
